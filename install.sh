5e2da423620a3cdedc56b7e620862185bd88c006
LunarOSX <Lunar.osx@proton.me>
2024-09-15 19:52:02 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+main() {
+    clear
+    echo -e "Welcome to the Lunar!"
+    echo -e "Install Script Version : 1.0.2"
+
+    echo -ne "Checking License..."
+    
+    echo -e " Done.\nWhitelist Due to Devloper"
+
+    echo -e "Downloading Lunar..."
+
+    echo -n "Installing Lunar... "
+
+    echo -e "Done"
+
+    echo -n "Updating Dylib..."
+    
+    echo -e " Done"
+    echo -e "Patching Lunar X Macsploit..."
+    echo -e "Done"
+    echo -e "Install Complete! Developed by Shadow_devz and Motocite!"
+    exit
+}
+
+main
