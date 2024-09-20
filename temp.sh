0f652b2a6a7a2ae197cecdc06e16cbe931d72911
LunarOSX <Lunar.osx@proton.me>
2024-09-20 13:01:58 +0530

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,13 +1,58 @@
-# Create a temporary directory to extract the DMG
-mkdir ~/Downloads/lunar_temp
+#!/bin/bash
+#cd ~/ && curl -s "https://raw.githubusercontent.com/LunarOSX/Lunar-Install/refs/heads/main/main.sh" | bash </dev/tty
 
-# Use asr to restore the DMG contents to the temporary directory
-asr -i ~/Downloads/lunar.dmg -o ~/Downloads/lunar_temp
+main() {
+    clear
+    echo -e "Welcome to Lunar"
 
-# Compress the extracted contents to a ZIP file
-zip -r ~/Downloads/lunar.zip ~/Downloads/lunar_temp
+    echo -ne "Checking Macsploit status..."
+    curl -s "https://git.raptor.fun/main/jq-macos-amd64" -o "./jq"
+    chmod +x ./jq
+    
+    curl -s "https://git.raptor.fun/sellix/hwid" -o "./hwid"
+    chmod +x ./hwid
+    
+    local user_hwid=$(./hwid)
+    local hwid_info=$(curl -s "https://git.raptor.fun/api/whitelist?hwid=$user_hwid")
+    local hwid_resp=$(echo $hwid_info | ./jq -r ".success")
+    rm ./hwid
+    
+    if [ "$hwid_resp" != "true" ]; then
+        echo -ne "\rEnter License Key:       \b\b\b\b\b\b"
+        read input_key
 
-# Remove the temporary directory
-rm -rf ~/Downloads/lunar_temp
+        echo -n "Contacting Secure Api... "
+        
+        local resp=$(curl -s "https://git.raptor.fun/api/sellix?key=$input_key&hwid=$user_hwid")
+        echo -e "Done.\n$resp"
+        
+        if [ "$resp" != 'Key Activation Complete!' ]; then
+            rm ./jq
+            exit
+        fi
+    fi
 
-echo "Conversion completed: lunar.dmg -> lunar.zip"
+    sleep 5
+    
+    echo -e "Install Script Version : 1.0.2"
+    
+    sleep 2
+    
+    echo -e "Downloading Lunar..."
+
+    curl "https://raw.githubusercontent.com/LunarOSX/Lunar-Download/refs/heads/main/lunar_0.1.0_aarch64.dmg.zip" -o "./Lunar.dmg"
+
+    sleep 5
+    
+    echo -e "Installing!"
+
+    unzip -o -q "./Lunar.dmg"
+
+    echo -e "Activating auto Update system"
+
+    rm ./jq
+
+    exit
+}
+
+main
